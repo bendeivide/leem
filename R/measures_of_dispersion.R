e6e4121097f38df607d32e051bc4ccdc2198b229
Ben Deivide <ben.deivide@gmail.com>
2022-05-23 18:52:49 -0300

    sdev

--- R/measures_of_dispersion.R
+++ R/measures_of_dispersion.R
@@ -1,41 +1,63 @@
 # Calculate of variance
 #' @export
-variance <- function(x, rounding = 2, na.rm = FALSE, details = FALSE) {
-  if (class(x) != "leem") stop("Use the 'new_leem()' function to create an object of class leem!", call. = FALSE)
-  if (class(x) == "leem" & is.null(attr(x, "table"))) x <- tabfreq(x)
+variance <- function (x, rounding = 2, na.rm = FALSE, details = FALSE, grouped = TRUE) {
+  if (class(x) != "leem")
+    stop("Use the 'new_leem()' function to create an object of class leem!",
+         call. = FALSE)
+  if (class(x) == "leem" & is.null(attr(x, "table")))
+    x <- tabfreq(x)
   if (attr(x, "variable") == "discrete") {
     numchar <- is.numeric(x$estat$raw_data)
     if (numchar) {
-      vari <- round(var(x = x$estat$raw_data,
-                        na.rm = na.rm), digits = rounding)
-      resume <- list(variance = vari, table = x$tabela, rawdata = x$estat$raw_data)
+      vari <- round(var(x = x$estat$raw_data, na.rm = na.rm),
+                    digits = rounding)
+      resume <- list(variance = vari, table = x$tabela,
+                     rawdata = x$estat$raw_data)
       if (details) {
         return(resume)
-      } else {
+      }
+      else {
         return(vari)
       }
-
-    } else {
-      stop("Measure not used for this data type!", call. = FALSE,
-           domain = "R-leem")
+    }
+    else {
+      stop("Measure not used for this data type!",
+           call. = FALSE, domain = "R-leem")
     }
   }
   if (attr(x, "variable") == "continuous") {
-    vari <- sum((x$tabela$PM - mean(x))^2 * x$tabela$Fi) / (x$estat$Numero_amostra - 1)
-    resume <- list(variance = vari, table = x$tabela, rawdata = x$estat$raw_data)
-    if (details) {
-      return(resume)
+    if (grouped == TRUE) {
+      vari <- sum((x$tabela$PM - mean(x))^2 * x$tabela$Fi)/(x$estat$Numero_amostra -
+                                                              1)
+      resume <- list(variance = vari, table = x$tabela, rawdata = x$estat$raw_data)
+      if (details) {
+        return(resume)
+      }
+      else {
+        return(vari)
+      }
     } else {
-      return(vari)
+      vari <- round(var(x = x$estat$raw_data, na.rm = na.rm),
+                    digits = rounding)
+      resume <- list(variance = vari, table = x$tabela,
+                     rawdata = x$estat$raw_data)
+      if (details) {
+        return(resume)
+      }
+      else {
+        return(vari)
+      }
+
     }
   }
-
 }
+
 
 
 # Standard Deviation
 #' @export
-sdev <- function(x, rounding = 2, na.rm = FALSE, details = FALSE) {
+sdev <- function(x, rounding = 2, na.rm = FALSE, details = FALSE,
+                 grouped = TRUE) {
   if (class(x) != "leem") stop("Use the 'new_leem()' function to create an object of class leem!", call. = FALSE)
   if (class(x) == "leem" & is.null(attr(x, "table"))) x <- tabfreq(x)
   if (attr(x, "variable") == "discrete") {
@@ -56,13 +78,27 @@ sdev <- function(x, rounding = 2, na.rm = FALSE, details = FALSE) {
     }
   }
   if (attr(x, "variable") == "continuous") {
-    desvpad <- sqrt((variance(x)))
-    resume <- list(sdeviation = desvpad, table = x$tabela, rawdata = x$estat$raw_data)
-    if (details) {
-      return(resume)
+    if (grouped == TRUE) {
+      desvpad <- sqrt((variance(x)))
+      resume <- list(sdeviation = desvpad, table = x$tabela,
+                     rawdata = x$estat$raw_data)
+      if (details) {
+        return(resume)
+      }
+      else {
+        return(desvpad)
+      }
     } else {
-      return(desvpad)
+      desvpad <- round(sd(x = x$estat$raw_data, na.rm = na.rm),
+                       digits = rounding)
+      resume <- list(sdeviation = desvpad, table = x$tabela,
+                     rawdata = x$estat$raw_data)
+      if (details) {
+        return(resume)
+      }
+      else {
+        return(desvpad)
+      }
     }
   }
-
 }
