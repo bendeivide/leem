cae182c499daf3d0c391579ef33702d76e189dc9
Ben Deivide <ben.deivide@gmail.com>
2022-05-23 18:10:15 -0300

    sdev

--- R/measures_of_dispersion.R
+++ R/measures_of_dispersion.R
@@ -31,3 +31,38 @@ variance <- function(x, rounding = 2, na.rm = FALSE, details = FALSE) {
   }
 
 }
+
+
+# Standard Deviation
+#' @export
+sdev <- function(x, rounding = 2, na.rm = FALSE, details = FALSE) {
+  if (class(x) != "leem") stop("Use the 'new_leem()' function to create an object of class leem!", call. = FALSE)
+  if (class(x) == "leem" & is.null(attr(x, "table"))) x <- tabfreq(x)
+  if (attr(x, "variable") == "discrete") {
+    numchar <- is.numeric(x$estat$raw_data)
+    if (numchar) {
+      desvpad <- round(sd(x = x$estat$raw_data,
+                          na.rm = na.rm), digits = rounding)
+      resume <- list(sdeviation = desvpad, table = x$tabela, rawdata = x$estat$raw_data)
+      if (details) {
+        return(resume)
+      } else {
+        return(desvpad)
+      }
+
+    } else {
+      stop("Measure not used for this data type!", call. = FALSE,
+           domain = "R-leem")
+    }
+  }
+  if (attr(x, "variable") == "continuous") {
+    desvpad <- sqrt((variance(x)))
+    resume <- list(sdeviation = desvpad, table = x$tabela, rawdata = x$estat$raw_data)
+    if (details) {
+      return(resume)
+    } else {
+      return(desvpad)
+    }
+  }
+
+}
