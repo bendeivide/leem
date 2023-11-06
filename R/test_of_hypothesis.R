1a10bcd569f0f916b760fb2e3bc141e0368a0e72
AndreBzF <obarbozafarias@gmail.com>
2023-11-06 08:59:00 -0300

    Graphics for test of hypotesys

--- R/test_of_hypothesis.R
+++ R/test_of_hypothesis.R
@@ -167,7 +167,7 @@ th <- function(x, y = NULL, test = "ztest", h0, prop = FALSE, p, pa, alternative
                           decision2 = decision2, conclusion = conclusion)
         }
       if (plot == TRUE) {
-        par(mar = c(20,4,4,4))
+        par(mar = c(10,4,4,4))
         x <- seq(- 5 * 1, ztab[1], by = 0.01)
         z <- seq(ztab[2],  5, by = 0.01)
         y <- seq( - 5, + 5, by = 0.01)
@@ -177,50 +177,62 @@ th <- function(x, y = NULL, test = "ztest", h0, prop = FALSE, p, pa, alternative
         curve(dnorm(x, mean = 0, sd = 1),  - 5 , + 5 ,
               ylim = c(0.0, 1.2 * max(fx,fy,fz)),xlab = "X",
               ylab = expression(f[X](x)),
-              panel.first = grid(col="gray90"))
+              panel.first = grid(col="gray90"), lwd = 5)
         polygon(c(y, rev(y)),
                 c(fy, rep(0, length(fy))),
-                col="#009E73")
+                col="#99ccff")
         polygon(c(x, rev(x)),
                 c(fx, rep(0, length(fx))),
-                col="red")
+                col="#cc0000")
         polygon(c(z,rev(z)), c(fz,rep(0,length(fz))),
-                col="red" )
-        abline(v = ztab[1], lty=2, col = "red")
-        abline(v = ztab[2], lty=2, col = "red")
-        text(-2.5, 0.3, "Critical point")
-        arrows(ztab[1] ,0.14, -2.3, 0.3)
-        text(2.5, 0.3, "Critical point")
-        arrows(ztab[2] ,0.14,2.3, 0.3)
-        axis(side = 1, at = c(ztab[1],ztab[2]),labels = c(ztab[1],ztab[2]), font = 2, col.axis = "red", col = "#009E73")
+                col="#cc0000" )
+        abline(v = ztab[1], lty=2, col = "#cc0000")
+        abline(v = ztab[2], lty=2, col = "#cc0000")
+        abline(v = ztest, lty=2, col = "blue")
+        text(-2.5, 0.3, expression(bold("Critical point")))
+        arrows(ztab[1] ,0.14, -2.3, 0.27)
+        text(2.5, 0.3, expression(bold("Critical point")))
+        arrows(ztab[2] ,0.14,2.3, 0.27)
+        text(-1.6, 0.4, expression(bold("zTest")))
+        arrows(ztest ,0.28, -1.4, 0.37)
+        aux2 <- par("usr")[3]-(par("usr")[4] - par("usr")[3])/20
+        axis(side = 1, at = c(ztab[1],ztab[2]), font = 2, labels = FALSE,
+             col.axis = "#cc0000", col.ticks = "#cc0000", col = "#559ee8")
         axis(side=1, at=as.character(c( - 5, ztab[1])), tick = TRUE, lwd = 1,
-             col="red", font = 2, lwd.ticks = 0, labels = FALSE)
+             col="#cc0000", font = 2, lwd.ticks = 0, labels = FALSE)
         axis(side=1, at=as.character(c(ztab[2], + 5)), tick = TRUE, lwd = 1,
-             col="red", font = 2, lwd.ticks = 0, labels = FALSE)
+             col="#cc0000", font = 2, lwd.ticks = 0, labels = FALSE)
+        axis(side = 1, at = c(ztab[1],ztab[2]), lwd = 0, 
+             col = "#cc0000", font = 2, tick = FALSE, col.axis="#cc0000", pos = aux2)
+        axis(side = 1, at = ztest, lwd = 0, 
+             col = "blue", font = 2, tick = TRUE, col.axis="blue", pos = aux2)
+        axis(side = 1, at = ztest, tick = TRUE, lwd = 1,
+             col="blue", lwd.ticks = 1)
+
         if(prop == TRUE){
           title("Test of proportion: Z-Test.")
         }else{
           title("Test of hypothesis: Z-Test.")
         }
-        text(0,0.3,"ACCEPT H0" )
-        text(ztab[1] - 1 , 0.1, "RRH0")
-        text(ztab[2] + 1 ,0.1,"RRH0")
-        abline(v = ztest, lty=2, col = "blue")
-        arrows(ztest, 0.05, 3, 0.19)
-        text(3.1,0.2, round(ztest, digits = 4))
-        mtext("Step 1: Hypothesis", side = 1, line = 5, adj = 0, col = "red")
-        mtext("___________________", side = 1, line = 5, adj = 0, col = "red")
-        mtext(t = nullhyp  , side = 1, line = 6, adj = 0)
-        mtext(t = althyp , side = 1, line = 7, adj = 0)
-        mtext("Step 2: Significante level ", side = 1, line = 10, adj = 0, col = "red")
-        mtext("______________________", side = 1, line = 10, adj = 0, col = "red")
-        mtext(t = signlevel  , side = 1, line = 11, adj = 0)
-        mtext("Step 3: Rule of decision ", side = 1, line = 5, adj = 1, col = "red")
-        mtext("______________________", side = 1, line = 5, adj = 1, col = "red")
-        mtext(decisionplot, side = 1, line = 6, adj = 1)
-        mtext("Step 4: Conclusion ", side = 1, line = 10, adj = 1, col = "red")
-        mtext("_________________", side = 1, line = 10, adj = 1, col = "red")
-        mtext(conclusionplot, side = 1, line = 11, adj = 1)
+        text(0,0.1,expression(bold("ACCEPT H0")), font = 2)
+        text(ztab[1] - 1 , 0.1, "RRH0", font = 2)
+        text(ztab[2] + 1 ,0.1,"RRH0", font = 2)
+        mtext("Step 1: Hypothesis", side = 1, line = 3, adj = 0, col = "#0099ff", font = 2)
+        mtext("________________", side = 1, line = 3, adj = 0, col = "#0099ff")
+        mtext(t = nullhyp  , side = 1, line = 4, adj = 0)
+        mtext(t = althyp , side = 1, line = 5, adj = 0)
+
+        mtext("Step 2: Significante level", side = 1, line = 6, adj = 0, col = "#0099ff", font = 2)
+        mtext("_____________________", side = 1, line = 6, adj = 0, col = "#0099ff")
+        mtext(t = signlevel  , side = 1, line = 7, adj = 0)
+
+        mtext("Step 3: Rule of decision", side = 1, line = 3, adj = 1, col = "#0099ff", font = 2)
+        mtext("____________________", side = 1, line = 3, adj = 1, col = "#0099ff")
+        mtext(decisionplot, side = 1, line = 4, adj = 1)
+
+        mtext("Step 4: Conclusion", side = 1, line = 6, adj = 1, col = "#0099ff", font = 2)
+        mtext("________________", side = 1, line = 6, adj = 1, col = "#0099ff")
+        mtext(conclusionplot, side = 1, line = 7, adj = 1)
       }
     }
     if (any(alternative == c("less", "l", "L"))) {
