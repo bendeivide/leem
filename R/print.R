00bd8284edcf99238a4b28f537f93d48e73de592
AndreBzF <obarbozafarias@gmail.com>
2023-12-20 21:11:47 -0300

    Implemented of ChiSquared test and padronization of th() function.

--- R/print.R
+++ R/print.R
@@ -40,8 +40,8 @@ output_confint <- function(x) {
 }
 
 output_htest <- function(x) {
-  if (x$test == "ztest") {
-    if(x$alternative == "two.sided"){
+  if (any(x$test ==  c("ztest", "z", "Z", "normal"))) {
+    if(any(x$alternative == c("two.sided", "t", "T"))){
       cat("\n\n", crayon::bgGreen$bold(x$title), "\n")
       # Step 1
       cat(crayon::blue$underline$bold(gettext("Step 1:", domain = "R-leem")),
@@ -124,8 +124,8 @@ output_htest <- function(x) {
       cat(crayon::bold(x$conclusion))
     }
   }
-  if (x$test == "ttest") {
-    if(x$alternative == "two.sided"){
+  if (any(x$test ==  c("ttest", "t", "T", "tstudent"))) {
+    if(any(x$alternative == c("two.sided", "t", "T"))){
       cat("\n\n", crayon::bgGreen$bold(x$title), "\n")
       # Step 1
       cat(crayon::blue$underline$bold(gettext("Step 1:", domain = "R-leem")),
@@ -208,7 +208,34 @@ output_htest <- function(x) {
       cat(crayon::bold(x$conclusion))
     }
   }
-  if (x$test == "chisq") {
+  if (any(x$test ==  c("chisqtest", "chisq", "CHI", "chisquared"))) {
+
+    if(any(x$alternative == c("two.sided", "t", "T"))){
+      cat("\n\n", crayon::bgGreen$bold(x$title), "\n")
+      # Step 1
+      cat(crayon::blue$underline$bold(gettext("Step 1:", domain = "R-leem")),
+          crayon::blue(gettext("Hypothesis", domain = "R-leem")), "\n")
+      cat(crayon::bold(x$nullhyp), "\n")
+      cat(crayon::bold(x$althyp), "\n\n")
+      # Step 2
+      cat(crayon::blue$underline$bold(gettext("Step 2:", domain = "R-leem")),
+          crayon::blue(gettext("Significance level", domain = "R-leem")), "\n")
+      cat(crayon::bold(x$signlevel), "\n\n")
+      # Step 3
+      cat(crayon::blue$underline$bold(gettext("Step 3:", domain = "R-leem")),
+          crayon::blue(gettext("Rule of decision", domain = "R-leem")), "\n")
+      cat(crayon::green$bold(gettext("   If |ST| > |CP| or |ST| < |CP|  => Reject H0!", domain = "R-leem")), "\n")
+      cat(crayon::green(gettext("   ST - Statistical Test; CP - Critical Point", domain = "R-leem")), "\n")
+      cat(crayon::green(gettext("So...", domain = "R-leem")), "\n")
+      cat(crayon::bold(x$decision), "\n")
+      cat(crayon::green(gettext("Otherside...", domain = "R-leem")), "\n")
+      cat(crayon::bold(x$decision2), "\n\n")
+
+      # Step 4
+      cat(crayon::blue$underline$bold(gettext("Step 4:", domain = "R-leem")),
+          crayon::blue(gettext("Conclusion", domain = "R-leem")), "\n")
+      cat(crayon::bold(x$conclusion))
+    }
 
     if(any(x$alternative == c("less","l","L"))){
 
@@ -225,8 +252,8 @@ output_htest <- function(x) {
       # Step 3
       cat(crayon::blue$underline$bold(gettext("Step 3:", domain = "R-leem")),
           crayon::blue(gettext("Rule of decision", domain = "R-leem")), "\n")
-      cat(crayon::green$bold(gettext("   If |TR| < |CP| => Reject H0!", domain = "R-leem")), "\n")
-      cat(crayon::green(gettext("   TR - Test Result; CP - Critical Point", domain = "R-leem")), "\n")
+      cat(crayon::green$bold(gettext("   If |ST| < |CP| => Reject H0!", domain = "R-leem")), "\n")
+      cat(crayon::green(gettext("   ST - Statistical Test; CP - Critical Point", domain = "R-leem")), "\n")
       cat(crayon::green(gettext("So...", domain = "R-leem")), "\n")
       cat(crayon::bold(x$decision), "\n")
       cat(crayon::green(gettext("Otherside...", domain = "R-leem")), "\n")
@@ -253,8 +280,8 @@ output_htest <- function(x) {
       # Step 3
       cat(crayon::blue$underline$bold(gettext("Step 3:", domain = "R-leem")),
           crayon::blue(gettext("Rule of decision", domain = "R-leem")), "\n")
-      cat(crayon::green$bold(gettext("   If |TR| > |CP| => Reject H0!", domain = "R-leem")), "\n")
-      cat(crayon::green(gettext("   TR - Test Result; CP - Critical Point", domain = "R-leem")), "\n")
+      cat(crayon::green$bold(gettext("   If |ST| > |CP| => Reject H0!", domain = "R-leem")), "\n")
+      cat(crayon::green(gettext("   ST - Statistical Test; CP - Critical Point", domain = "R-leem")), "\n")
       cat(crayon::green(gettext("So...", domain = "R-leem")), "\n")
       cat(crayon::bold(x$decision), "\n")
       cat(crayon::green(gettext("Otherside...", domain = "R-leem")), "\n")
@@ -265,6 +292,10 @@ output_htest <- function(x) {
           crayon::blue(gettext("Conclusion", domain = "R-leem")), "\n")
       cat(crayon::bold(x$conclusion))
     }
+  }
+  if (any(x$test ==  c("ftest", "f", "F"))){
+
+    stop("Em desenvolvimento", .call = FALSE, domain = "R-leem")
   }
 }
 output_table <- function(x) {
