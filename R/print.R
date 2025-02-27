b5c87a9812cb66c29a1be316698758cc314afded
bendeivide <ben.deivide@gmail.com>
2025-02-27 19:50:17 -0300

    Teste de hipotese para uma amostra - teste bilateral

--- R/print.R
+++ R/print.R
@@ -42,7 +42,7 @@ output_confint <- function(x) {
 output_htest <- function(x) {
   if (x$test == "ztest") {
     if(x$alternative == "two.sided"){
-      cat("\n\n", crayon::bgGreen$bold(x$title), "\n")
+      cat("\n\n", crayon::bgRed$bold(x$title), "\n")
       # Step 1
       cat(crayon::blue$underline$bold(gettext("Step 1:", domain = "R-leem")),
           crayon::blue(gettext("Hypothesis", domain = "R-leem")), "\n")
@@ -55,11 +55,11 @@ output_htest <- function(x) {
       # Step 3
       cat(crayon::blue$underline$bold(gettext("Step 3:", domain = "R-leem")),
           crayon::blue(gettext("Rule of decision", domain = "R-leem")), "\n")
-      cat(crayon::green$bold(gettext("   If |ztest| > |ztab| => Reject H0!", domain = "R-leem")), "\n")
-      cat(crayon::green(gettext("   ztest - test statistic; ztab - critical point", domain = "R-leem")), "\n")
+      cat("  ", crayon::green$bold(gettext("If |ztest| > |ztab| => Reject H0!", domain = "R-leem")), "\n")
+      cat("  ", crayon::green(gettext("ztest - test statistic; ztab - critical point", domain = "R-leem")), "\n")
       cat(crayon::green(gettext("So...", domain = "R-leem")), "\n")
       cat(crayon::bold(x$decision), "\n")
-      cat(crayon::green(gettext("Otherside...", domain = "R-leem")), "\n")
+      cat(crayon::green(gettext("Otherwise...", domain = "R-leem")), "\n")
       cat(crayon::bold(x$decision2), "\n\n")
 
       # Step 4
